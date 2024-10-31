ea658303384be34feb62353b64148178c5882faf
Reza <muhammadrezayanuarhamdani47@gmail.com>
2024-10-31 18:30:46 +0700

    Adjust layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,43 +8,46 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    
 </head>
-<body class="bg-gray-100">
+<body>
     <header class="flex justify-center fixed top-0 left-0 right-0 z-10">
-        <div class="bg-[#8F8B55] text-white my-4 flex items-center font-lora rounded-3xl px-8">
+  
+        <div class="bg-[#8F8B55] text-white my-10 flex justify-start font-lora w-fit rounded-3xl px-8">
+    
             <p class="my-2 py-2 px-6 mx-2 bg-gray-400/60 rounded-3xl">Home</p>
 
-            <a href="{{ route('reservation.index') }}">
-                <p class="my-2 py-2 px-6 mx-2">Order</p>
-            </a>
+        <a href="{{route('reservation.index')}}">
+            <p class="my-2 py-2 px-6 mx-2">Order</p>
+        </a>
 
             <p class="my-2 py-2 px-6 mx-2">About</p>
+            
         </div>
         
-        <div class="ml-auto">
-            <div class="dropdown relative" aria-labelledby="navbarDropdown">
-                <a class="text-white ml-4" href="{{ route('logout') }}"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                    @csrf
-                </form>
-            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </div>
     </header>
-
-    <main class="flex-grow mt-20">
+    <div class="flex-grow">
         @yield('content')
-    </main>
-
+    
+    </div>
     <footer class="mt-10 bg-gray-800 text-white py-4">
-        <img src="{{ asset('images/IMG-20240508-WA0064.jpg') }}" alt="logo" class="mx-auto w-40 h-30">
+        <img src="{{ asset('images/IMG-20240508-WA0064.jpg') }}" alt="logo" class="w-40 h-30">
         <div class="container mx-auto text-center">
-            <p>&copy; 2024 Reservasi Rumah. All Rights Reserved.</p>
+          <p>&copy; 2024 Reservasi Rumah. All Rights Reserved.</p>
         </div>
-    </footer>
+      </footer>
 </body>
 </html>
