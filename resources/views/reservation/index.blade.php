6c67023098d1e20f33fced6837041cc61a2d9115
Reza <muhammadrezayanuarhamdani47@gmail.com>
2024-10-31 18:02:24 +0700

    Adjust reservation.index

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Reservasi Pembelian Rumah</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])</head>
-<body class="bg-gray-100">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <body class="bg-gray-100">
 
   <!-- Navbar -->
   <nav class="bg-white shadow-md">
