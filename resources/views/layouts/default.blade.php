f7abdbbfa85c9989c6a7343fcfd0a906206b709d
ferdyawan <ferdy19ndy@gmail.com>
2024-11-14 11:47:55 +0700

    Adjust Navbar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,41 +13,53 @@
     
 </head>
 <body>
-    <header class="flex justify-center fixed top-0 left-0 right-0 z-0">
-  
-        <div class="bg-[#8F8B55] text-white my-10 flex justify-start font-lora w-fit rounded-3xl px-8">
-            
-        <a href="/" class="home-link">
-            <p class="my-2 py-2 px-6 mx-2  bg-gray-400/60 rounded-3xl" id="home-text">Home</p>
-        </a>
-
-        <a href="/houses" class="projects-link">
-            <p class="my-2 py-2 px-6 mx-2" id="projects-text">Projects</p>
-        </a>
-
-        <a href="/contact">
-            <p class="my-2 py-2 px-6 mx-2">Contact</p>
-        </a>
-        </div>
-        
-        @auth
-        <div class="py-12 px-4 fixed top-0 right-0 z-10" >
-            <a class="  bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+    <header class="flex justify-center item-center fixed top-0 left-0 right-0 z-0">
+        <nav class="bg-[#8F8B55] fixed w-full z-20 top-0 start-0">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="{{ asset('image/logo.png') }}" class="h-9" alt="Logo">
             </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </div>
-        @endauth
-        
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <a href="/contact" class="text-white bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Chat Langsung</a>
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+                <li>
+                <a href="/" class="block py-2 px-3 text-white  rounded md:bg-transparent ">Home</a>
+                </li>
+                <li>
+                <a href="/houses" class="block py-2 px-3 text-white">Projects</a>
+                </li>
+                <li>
+                <a href="/contact" class="block py-2 px-3 text-white">Contact</a>
+                </li>
+            </ul>
+            </div>
+            </div>
+            @auth
+            <div class="py-12 px-4 fixed top-0 right-0 z-10" >
+                <a class="  bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+    
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+            @endauth
+        </nav>
     </header>
     <main class="flex-grow">
         @yield('content')
-        <footer class="mt-10 bg-gray-800 text-white py-4 fixed bottom-0 left-0 w-full">
+        <footer class="mt-10 bg-gray-800 text-white py-4 flex bottom-0 left-0 w-full">
             <img src="{{ asset('image/logo.png') }}" alt="logo" class="w-40 h-30">
             <div class="container mx-auto text-center">
             <p>&copy; 2024 Reservasi Rumah. All Rights Reserved.</p>
