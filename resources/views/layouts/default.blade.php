0270326d35c89a6a9d93257561a10ae41b63fc5b
Reza <muhammadrezayanuarhamdani47@gmail.com>
2024-12-08 23:35:40 +0700

    adjust layout.default

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- to allow mixed content --}}
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reka Kreasi Bumi</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
