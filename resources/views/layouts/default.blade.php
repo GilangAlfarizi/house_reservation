9e447be2585c4b7cca54106cf8c2b5b048fbddd1
Reza <muhammadrezayanuarhamdani47@gmail.com>
2024-12-08 23:21:19 +0700

    adjust layout default

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -16,7 +16,7 @@
     <header class="flex justify-center item-center fixed top-0 left-0 right-0 z-0">
         <nav class="bg-[#8F8B55] fixed w-full z-20 top-0 start-0">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="{{ asset('image/logo.png') }}" class="h-9" alt="Logo">
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
